1c3051b6590531fb793cdcb46c244454bb53e2a6
solinemignot <soline.mignot@ensae.fr>
2025-11-13 20:35:39 +0100

    y'a la grille pour les photos sur la page 2!!

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import json
+import random 
+
+#Import des photos des personnages
+from static.perso_images import personnages
+
+#Import des caractéristiques des personnages
+from static.perso_caract import caracteristiques_personnages
+
 
 app = Flask(__name__)
 
@@ -7,7 +16,25 @@ app = Flask(__name__)
 def accueil():
     return render_template('accueil.html')
 
-@app.route('/jeu')
-def jeu():
-    return render_template('jeu.html')
 
+@app.route('/jeu', methods=['GET', 'POST'])
+def jeu():
+    #L'algorithme choisit au hasard un personnage qu'il faudra deviner
+    perso = random.choice(list(caracteristiques_personnages.keys()))
+
+    print(perso)
+    reponse = None
+    if request.method == 'POST':
+        question = request.form.get('question').lower()
+        reponse = traiter_question(question, perso)
+    return render_template('jeu.html', personnages=personnages, reponse=reponse)
+
+def traiter_question(question, perso):
+    if "lunettes" in question:
+        avec_lunettes = [nom for nom, img in personnages.items() if personnages_info[nom]["lunettes"]]
+        return f"Personnages avec des lunettes : {', '.join(avec_lunettes)}"
+    # Add more rules here
+    return "Je n'ai pas compris la question."
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/perso_images.py
@@ -0,0 +1,40 @@
+personnages = {
+    "Harry Potter": "https://static.wikia.nocookie.net/harrypotter/images/4/4a/Harry_Potter_-_OOTP_promo.jpg",
+    "Hermione Granger": "https://upload.wikimedia.org/wikipedia/en/d/d3/Hermione_Granger_poster.jpg",
+    "Ron Weasley": "https://upload.wikimedia.org/wikipedia/en/5/5e/Ron_Weasley_poster.jpg",
+    "Draco Malfoy": "https://static.wikia.nocookie.net/harrypotter/images/2/20/Draco_Malfoy_DH2.jpg",
+    "Luna Lovegood": "https://static.wikia.nocookie.net/harrypotter/images/9/99/LunaLovegood-WB-F5-LunaLovegoodPromoStill-001.jpg",
+    "Neville Londubat": "https://static.wikia.nocookie.net/harrypotter/images/b/b1/Neville_in_DH2.jpg",
+    "Ginny Weasley": "https://static.wikia.nocookie.net/harrypotter/images/a/ad/Ginny_Weasley_COS_promo.jpg",
+    "Fred Weasley": "https://static.wikia.nocookie.net/harrypotter/images/4/4d/Fred_Weasley_WB_F5_FredWeasleyPromo.jpg",
+    "George Weasley": "https://static.wikia.nocookie.net/harrypotter/images/1/10/George_Weasley_WB_F5_GeorgeWeasleyPromo.jpg",
+    "Severus Rogue": "https://static.wikia.nocookie.net/harrypotter/images/5/5e/Snape_HBP_promo.jpg",
+    "Minerva McGonagall": "https://static.wikia.nocookie.net/harrypotter/images/1/12/Professor_McGonagall_DH_promo.jpg",
+    "Albus Dumbledore": "https://static.wikia.nocookie.net/harrypotter/images/0/0e/Dumbledore-HBP_promo.jpg",
+    "Rubeus Hagrid": "https://static.wikia.nocookie.net/harrypotter/images/2/29/RubeusHagrid_WB_F1_HagridHoldingUmbrella_Still_080615_Port.jpg",
+    "Sirius Black": "https://static.wikia.nocookie.net/harrypotter/images/d/df/Sirius_Black_-_OOTP_promo.jpg",
+    "Remus Lupin": "https://static.wikia.nocookie.net/harrypotter/images/3/3e/Remus_Lupin_OOTP_promo.jpg",
+    "Bellatrix Lestrange": "https://static.wikia.nocookie.net/harrypotter/images/1/14/Bellatrix_Lestrange_BH_promo.jpg",
+    "Dolores Ombrage": "https://static.wikia.nocookie.net/harrypotter/images/7/75/Dolores_Umbridge_OOTP_promo.jpg",
+    "Cedric Diggory": "https://static.wikia.nocookie.net/harrypotter/images/0/0a/Cedric_Diggory_GOF_promo.jpg",
+    "Cho Chang": "https://static.wikia.nocookie.net/harrypotter/images/a/a2/Cho_Chang_OOTP_promo.jpg",
+    "Peter Pettigrow": "https://static.wikia.nocookie.net/harrypotter/images/6/65/Wormtail_promo_OOTP.jpg"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
