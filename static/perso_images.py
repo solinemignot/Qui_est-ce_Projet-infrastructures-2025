7ac10fc5f14548a0351a73b6f81f521b4e7f8218
Leena Boyina <lboyina@yahoo.fr>
2025-11-15 16:51:52 +0100

    ajout des images en local (pas de pb de droits d'auteurs et ajustement de la grille pour un rendu harmonieux

--- static/perso_images.py
+++ static/perso_images.py
@@ -1,25 +1,26 @@
 personnages = {
-    "Harry Potter": "https://static.wikia.nocookie.net/harrypotter/images/4/4a/Harry_Potter_-_OOTP_promo.jpg",
-    "Hermione Granger": "https://upload.wikimedia.org/wikipedia/en/d/d3/Hermione_Granger_poster.jpg",
-    "Ron Weasley": "https://upload.wikimedia.org/wikipedia/en/5/5e/Ron_Weasley_poster.jpg",
-    "Draco Malfoy": "https://static.wikia.nocookie.net/harrypotter/images/2/20/Draco_Malfoy_DH2.jpg",
-    "Luna Lovegood": "https://static.wikia.nocookie.net/harrypotter/images/9/99/LunaLovegood-WB-F5-LunaLovegoodPromoStill-001.jpg",
-    "Neville Londubat": "https://static.wikia.nocookie.net/harrypotter/images/b/b1/Neville_in_DH2.jpg",
-    "Ginny Weasley": "https://static.wikia.nocookie.net/harrypotter/images/a/ad/Ginny_Weasley_COS_promo.jpg",
-    "Fred Weasley": "https://static.wikia.nocookie.net/harrypotter/images/4/4d/Fred_Weasley_WB_F5_FredWeasleyPromo.jpg",
-    "George Weasley": "https://static.wikia.nocookie.net/harrypotter/images/1/10/George_Weasley_WB_F5_GeorgeWeasleyPromo.jpg",
-    "Severus Rogue": "https://static.wikia.nocookie.net/harrypotter/images/5/5e/Snape_HBP_promo.jpg",
-    "Minerva McGonagall": "https://static.wikia.nocookie.net/harrypotter/images/1/12/Professor_McGonagall_DH_promo.jpg",
-    "Albus Dumbledore": "https://static.wikia.nocookie.net/harrypotter/images/0/0e/Dumbledore-HBP_promo.jpg",
-    "Rubeus Hagrid": "https://static.wikia.nocookie.net/harrypotter/images/2/29/RubeusHagrid_WB_F1_HagridHoldingUmbrella_Still_080615_Port.jpg",
-    "Sirius Black": "https://static.wikia.nocookie.net/harrypotter/images/d/df/Sirius_Black_-_OOTP_promo.jpg",
-    "Remus Lupin": "https://static.wikia.nocookie.net/harrypotter/images/3/3e/Remus_Lupin_OOTP_promo.jpg",
-    "Bellatrix Lestrange": "https://static.wikia.nocookie.net/harrypotter/images/1/14/Bellatrix_Lestrange_BH_promo.jpg",
-    "Dolores Ombrage": "https://static.wikia.nocookie.net/harrypotter/images/7/75/Dolores_Umbridge_OOTP_promo.jpg",
-    "Cedric Diggory": "https://static.wikia.nocookie.net/harrypotter/images/0/0a/Cedric_Diggory_GOF_promo.jpg",
-    "Cho Chang": "https://static.wikia.nocookie.net/harrypotter/images/a/a2/Cho_Chang_OOTP_promo.jpg",
-    "Peter Pettigrow": "https://static.wikia.nocookie.net/harrypotter/images/6/65/Wormtail_promo_OOTP.jpg"
+    "Albus Dumbledore": "/static/images/albus_dumbledore.jpg",
+    "Bellatrix Lestrange": "/static/images/bellatrix_lestrange.jpg",
+    "Cedric Diggory": "/static/images/cedric_diggory.jpg",
+    "Cho Chang": "/static/images/Cho_Chang.jpg",
+    "Dolores Ombrage": "/static/images/dolores_ombrage.jpg",
+    "Drago Malefoy": "/static/images/draco_malfoy.jpg",
+    "Fred Weasley": "/static/images/fred_weasley.jpg",
+    "George Weasley": "/static/images/george_weasley.jpg",
+    "Ginny Weasley": "/static/images/ginny_weasley.jpg",
+    "Harry Potter": "/static/images/harry_potter.jpg",
+    "Hermione Granger": "/static/images/Hermione_Granger.jpg",
+    "Luna Lovegood": "/static/images/Luna_Lovegood.jpg",
+    "Minerva McGonagall": "/static/images/Minerva_McGonagall.jpg",
+    "Neville Londubat": "/static/images/Neville_londubat.jpg",
+    "Peter Pettigrew": "/static/images/Peter_Pettigrew.jpg",
+    "Remus Lupin": "/static/images/Remus_Lupin.jpg",
+    "Ron Weasley": "/static/images/Ron_Weasley.jpg",
+    "Rubeus Hagrid": "/static/images/Rubeus_Hagrid.jpg",
+    "Severus Rogue": "/static/images/severus_rogue.jpg",
+    "Sirius Black": "/static/images/Sirius_Black.jpg"
 }
+
 
 
 
